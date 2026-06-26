e99623cdf951bb5a994fc85adf276a8842b56bc2
Noah Pope <npope@tutanota.com>
2026-06-26 09:00:32 -0500

    adding promptUser message to class

--- CS106A_InchesToCentimeters.java
+++ CS106A_InchesToCentimeters.java
@@ -1,7 +1,7 @@
 /**
  * File: CS106A_InchesToCentimeters.java
  * ----------------------------
- * This program asks the user to enter a number of inches
+ * This Java program asks the user to enter a number of inches
  * as a floating point number which then gets converted into 
  * centimeters.
  */
@@ -10,8 +10,20 @@ import acm.program.*;
 
 public class CS106A_InchesToCentimeters extends ConsoleProgram
 {
+	private static final double CENTIMETERS_PER_INCH = 2.54;
+	
 	public void run()
 	{ 
-		
+		promptUser();
+	}
+	
+	
+	private void promptUser()
+	{
+		println("This program converts inches to centimeters.");
+		double inches = readDouble("Enter value inches: ");
+		double cm = inches * CENTIMETERS_PER_INCH;
+		println(inches + "in = " + cm + "cm");
+		println("donezo");
 	}
 }
