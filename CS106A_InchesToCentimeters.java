6f8fbe58a37914b701831a52e3e6cb7fadcacb46
Noah Pope <npope@tutanota.com>
2026-06-26 08:53:37 -0500

    adding files to new repo

---
+++ CS106A_InchesToCentimeters.java
@@ -0,0 +1,17 @@
+/**
+ * File: CS106A_InchesToCentimeters.java
+ * ----------------------------
+ * This program asks the user to enter a number of inches
+ * as a floating point number which then gets converted into 
+ * centimeters.
+ */
+
+import acm.program.*;
+
+public class CS106A_InchesToCentimeters extends ConsoleProgram
+{
+	public void run()
+	{ 
+		
+	}
+}
